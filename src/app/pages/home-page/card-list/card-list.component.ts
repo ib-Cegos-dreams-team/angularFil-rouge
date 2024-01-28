6de6c092b25b35469641f0d5a9ab7e45994509f2
fmoscatiello <farid.moscatiello@outlook.fr>
2024-01-28 16:29:26 +0100

    Ajout du carousel avec les cards dans la homepage

--- src/app/pages/home-page/card-list/card-list.component.ts
+++ src/app/pages/home-page/card-list/card-list.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
+import { CardComponent } from "../card/card.component";
 
 @Component({
-  selector: 'app-card-list',
-  standalone: true,
-  imports: [],
-  templateUrl: './card-list.component.html',
-  styleUrl: './card-list.component.css'
+    selector: 'app-card-list',
+    standalone: true,
+    templateUrl: './card-list.component.html',
+    styleUrl: './card-list.component.css',
+    imports: [CardComponent]
 })
 export class CardListComponent {
 
