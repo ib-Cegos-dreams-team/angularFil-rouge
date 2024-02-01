5d8479630696952d519a5fa9420117e89fa51fa8
parshimipopeli <philippe.lamorskipro@gmail.com>
2024-02-01 15:02:30 +0100

    ajout de la page listFormation

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,9 +1,11 @@
 import { Routes } from '@angular/router';
 import { HomePageComponent } from './pages/home-page/home-page.component';
 import { NotFoundComponent } from './commons/not-found/not-found.component';
+import { FormationsListComponent } from './pages/formations-page/formations-list/formations-list.component';
 
 export const routes: Routes = [
     { path: '', redirectTo: 'home', pathMatch: 'full' },
     { path: 'home', component: HomePageComponent },
+    { path: 'formations', component: FormationsListComponent },
     { path: '**', component: NotFoundComponent }
 ];
