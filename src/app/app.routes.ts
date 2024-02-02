e2007ee310392f6c9f2ff7c0ed515f1a88a49a1c
parshimipopeli <philippe.lamorskipro@gmail.com>
2024-02-02 11:53:51 +0100

    Ajout de la page formation-theme, formation details

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,10 +2,14 @@ import { Routes } from '@angular/router';
 import { HomePageComponent } from './pages/home-page/home-page.component';
 import { NotFoundComponent } from './commons/not-found/not-found.component';
 import { FormationsListComponent } from './pages/formations-page/formations-list/formations-list.component';
+import { FormationThemeComponent} from './pages/formations-page/formation-theme/formation-theme.component';
+import { FormationDetailsComponent } from './pages/formations-page/formation-details/formation-details.component';
 
 export const routes: Routes = [
     { path: '', redirectTo: 'home', pathMatch: 'full' },
     { path: 'home', component: HomePageComponent },
     { path: 'formations', component: FormationsListComponent },
+    { path: 'formations/formation-theme', component: FormationThemeComponent },
+    { path: 'formations/formation-theme/formation-details', component: FormationDetailsComponent },
     { path: '**', component: NotFoundComponent }
 ];
--- src/app/pages/formations-page/formation-details/formation-details.component.ts
+++ src/app/pages/formations-page/formation-details/formation-details.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
+import { RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-formation-details',
   standalone: true,
-  imports: [],
+  imports: [RouterModule],
   templateUrl: './formation-details.component.html',
   styleUrl: './formation-details.component.css'
 })
