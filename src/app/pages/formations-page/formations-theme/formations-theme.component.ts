95c8ed8aeda3dd01b5ea52062083171adf5ee8e2
parshimipopeli <philippe.lamorskipro@gmail.com>
2024-02-05 12:31:32 +0100

    Ajout page formationList, formationDetail

---
+++ src/app/pages/formations-page/formations-theme/formations-theme.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import { RouterModule } from '@angular/router';
+
+@Component({
+  selector: 'app-formations-theme',
+  standalone: true,
+  imports: [RouterModule],
+  templateUrl: './formations-theme.component.html',
+  styleUrl: './formations-theme.component.css'
+})
+export class FormationsThemeComponent {
+
+}
