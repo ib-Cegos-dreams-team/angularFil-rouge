7534bca8f5f5107f204d13b6fa3dc3a917fe58cd
fmoscatiello <farid.moscatiello@outlook.fr>
2024-02-06 09:25:33 +0100

    modification de la route pour la page formations

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,15 +2,15 @@ import { Routes } from '@angular/router';
 import { HomePageComponent } from './pages/home-page/home-page.component';
 import { NotFoundComponent } from './commons/not-found/not-found.component';
 import { CreationCompteComponent } from './pages/compte/creation-compte/creation-compte.component';
-import { FormationsListComponent } from './pages/formations-page/formations-list/formations-list.component';
 import { FormationDetailsComponent } from './pages/formations-page/formation-details/formation-details.component';
 import { FormationsThemeComponent } from './pages/formations-page/formations-theme/formations-theme.component';
+import { FormationsPageComponent } from './pages/formations-page/formations-page.component';
 
 export const routes: Routes = [
     { path: '', redirectTo: 'home', pathMatch: 'full' },
     { path: 'home', component: HomePageComponent },
     { path: 'creation-compte', component: CreationCompteComponent},
-    { path: 'formations', component: FormationsListComponent },
+    { path: 'formations', component: FormationsPageComponent },
     { path: 'formations/formation-theme', component: FormationsThemeComponent },
     { path: 'formations/formation-theme/formation-details', component: FormationDetailsComponent },
     { path: '**', component: NotFoundComponent }
--- src/app/pages/formations-page/formations-page.component.ts
+++ src/app/pages/formations-page/formations-page.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
+import { FormationsListComponent } from "./formations-list/formations-list.component";
 
 @Component({
-  selector: 'app-formations-page',
-  standalone: true,
-  imports: [],
-  templateUrl: './formations-page.component.html',
-  styleUrl: './formations-page.component.css'
+    selector: 'app-formations-page',
+    standalone: true,
+    templateUrl: './formations-page.component.html',
+    styleUrl: './formations-page.component.css',
+    imports: [FormationsListComponent]
 })
 export class FormationsPageComponent {
 
