522629cb50d8edc47048ab985d8443d8d2f3e223
parshimipopeli <philippe.lamorskipro@gmail.com>
2024-02-07 14:36:54 +0100

    changement formComponent.ts

--- src/app/pages/compte/creation-compte/creation-compte.component.ts
+++ src/app/pages/compte/creation-compte/creation-compte.component.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import { FormControl, FormGroup, FormsModule, NgForm, ReactiveFormsModule, Validators } from '@angular/forms';
+import { FormBuilder, FormControl, FormGroup, FormsModule, NgForm, ReactiveFormsModule, Validators } from '@angular/forms';
 import { confirmPasswordValidator } from '../../../confirm-password-validator';
 import { CommonModule } from '@angular/common';
 
@@ -11,18 +11,19 @@ import { CommonModule } from '@angular/common';
   styleUrl: './creation-compte.component.css'
 })
 export class CreationCompteComponent {
-  user = new FormGroup({
-    gender: new FormControl('', Validators.required),
-    firstName: new FormControl('', Validators.required),
-    lastName: new FormControl('',[Validators.required, Validators.minLength(3), Validators.maxLength(15)] ),
-    phone: new FormControl('', Validators.required),
-    address: new FormControl('', Validators.required),
-    email: new FormControl('', Validators.required),
-    password: new FormControl('', [Validators.required, Validators.minLength(5), Validators.maxLength(50)]),
-    confirmPassword: new FormControl('', [Validators.required, Validators.minLength(5), Validators.maxLength(50)]),
+  user = this.formBuilder.group({
+    gender: ['', Validators.required],
+    firstName: ['', [Validators.required, Validators.minLength(3), Validators.maxLength(15)]],
+    lastName: ['',[Validators.required, Validators.minLength(3), Validators.maxLength(15)]] ,
+    phone: ['', Validators.required],
+    address: ['', Validators.required],
+    email: ['', Validators.required],
+    password: ['', [Validators.required, Validators.minLength(5), Validators.maxLength(50)]],
+    confirmPassword: ['', [Validators.required, Validators.minLength(5), Validators.maxLength(50)]],
    
   },  { validators : confirmPasswordValidator }
   );
+  constructor(private formBuilder: FormBuilder) { }
 
 onSubmit() {
   console.log(this.user.value)
