0f1cbdcddd908f88e8018bee05a1f15767267b17
parshimipopeli <philippe.lamorskipro@gmail.com>
2024-02-07 11:44:04 +0100

    creation confirmPasswordValidator

--- src/app/pages/compte/creation-compte/creation-compte.component.ts
+++ src/app/pages/compte/creation-compte/creation-compte.component.ts
@@ -1,27 +1,30 @@
 import { Component } from '@angular/core';
-import { FormBuilder, FormGroup, FormsModule, ReactiveFormsModule, Validators } from '@angular/forms';
+import { FormControl, FormGroup, FormsModule, NgForm, ReactiveFormsModule, Validators } from '@angular/forms';
+import { confirmPasswordValidator } from '../../../confirm-password-validator';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-creation-compte',
   standalone: true,
-  imports: [FormsModule, ReactiveFormsModule],
+  imports: [ReactiveFormsModule, CommonModule],
   templateUrl: './creation-compte.component.html',
   styleUrl: './creation-compte.component.css'
 })
 export class CreationCompteComponent {
-  user = {
-    gender: '',
-    firstName: '',
-    lastName: '',
-    phone: '',
-    address: '',
-    email: '',
-    password: '',
-    confirmPassword: ''
-  };
+  user = new FormGroup({
+    gender: new FormControl('', Validators.required),
+    firstName: new FormControl('', Validators.required),
+    lastName: new FormControl('',[Validators.required, Validators.minLength(3), Validators.maxLength(15)] ),
+    phone: new FormControl('', Validators.required),
+    address: new FormControl('', Validators.required),
+    email: new FormControl('', Validators.required),
+    password: new FormControl('', [Validators.required, Validators.minLength(5), Validators.maxLength(50)]),
+    confirmPassword: new FormControl('', [Validators.required, Validators.minLength(5), Validators.maxLength(50)]),
+   
+  },  { validators : confirmPasswordValidator }
+  );
 
-  onSubmit() {
-    console.log('Form submitted:', this.user);
-    // You can add further logic here, like sending the data to a server
-  }
+onSubmit() {
+  console.log(this.user.value)
+}
 }
