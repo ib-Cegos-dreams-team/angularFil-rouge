a226f14b7fde9f0b3d4003fd2d21b5f84b8d9d62
Adrien <adriensauvage59@gmail.com>
2024-02-15 19:32:19 +0100

    ajout Admin Guard qui protege les routes admin, modif des methodes de connections qui va stocker le role de l'utilisateur

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -12,13 +12,14 @@ import { ConnexionPageComponent } from './pages/compte/connexion-page/connexion-
 import { AdminPageComponent } from './pages/admin-page/admin-page.component';
 import { AjouterUtilisateurComponent } from './pages/admin-page/ajouter-utilisateur/ajouter-utilisateur.component';
 import { SupprimerUtilisateurComponent } from './pages/admin-page/supprimer-utilisateur/supprimer-utilisateur.component';
+import { AdminGuard } from './guard/admin.guard';
 
 export const routes: Routes = [
     { path: '', redirectTo: 'home', pathMatch: 'full' },
     { path: 'home', component: HomePageComponent },
-    { path: 'admin', component: AdminPageComponent},
-    { path: 'admin/ajouter-utilisateur', component: AjouterUtilisateurComponent},
-    { path: 'admin/supprimer-utilisateur', component: SupprimerUtilisateurComponent},
+    { path: 'admin', component: AdminPageComponent, canActivate: [AdminGuard]},
+    { path: 'admin/ajouter-utilisateur', component: AjouterUtilisateurComponent, canActivate: [AdminGuard]},
+    { path: 'admin/supprimer-utilisateur', component: SupprimerUtilisateurComponent, canActivate: [AdminGuard]},
     { path: 'identification', component: ConnexionPageComponent},
     { path: 'identification/creation-compte', component: CreationCompteComponent},
     { path: 'formations', component: FormationsPageComponent },
