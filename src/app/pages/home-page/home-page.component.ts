6fe06c4cc7cd06271e001ce3ae84adaaba6c4152
parshimipopeli <philippe.lamorskipro@gmail.com>
2024-02-15 16:00:24 +0100

    ajout du formulaire de demande de formation personalisée

--- src/app/pages/home-page/home-page.component.ts
+++ src/app/pages/home-page/home-page.component.ts
@@ -1,13 +1,14 @@
 import { Component } from '@angular/core';
 import { BannerComponent } from "./banner/banner.component";
 import { CardListComponent } from "./card-list/card-list.component";
+import { FormationPersonnaliseeComponent } from "../../commons/formation-personnalisee/formation-personnalisee.component";
 
 @Component({
     selector: 'app-home-page',
     standalone: true,
     templateUrl: './home-page.component.html',
     styleUrl: './home-page.component.css',
-    imports: [BannerComponent, CardListComponent]
+    imports: [BannerComponent, CardListComponent, FormationPersonnaliseeComponent]
 })
 export class HomePageComponent {
 
