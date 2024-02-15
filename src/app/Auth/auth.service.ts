da7186ec82d444e11ef27066718f1996bf952474
AdriSauv <adriensauvage59@gmail.com>
2024-02-15 09:04:25 +0100

    correction bug import

--- src/app/Auth/auth.service.ts
+++ src/app/Auth/auth.service.ts
@@ -1,6 +1,6 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
-import { Observable } from 'rxjs';
+import { BehaviorSubject, Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
@@ -10,6 +10,10 @@ export class AuthService {
   private apiUrl = 'http://localhost:8080/auth'
 
   constructor(public http: HttpClient) { }
+
+  authSubject = new BehaviorSubject<any>({
+    user:null
+  });
 
   enregistrer(user:any):Observable<any>{
     return this.http.post<any>(`${this.apiUrl}/signup`, user)
